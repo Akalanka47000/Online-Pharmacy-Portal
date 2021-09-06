31539d63a29712070611dd40a65b9e0605bdeb33
Akalanka Perera <akalankaperera128@gmail.com>
2021-09-07 00:47:02 +0530

    Landing page completed

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,14 @@
-let index = 0;
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(showSlides, 4000);
+  particlesJS.load(
+    "particles-js",
+    "/Online-Pharmacy-Portal/assets/packages/particles/particles.json"
+  );
+  initVisibilities();
+  renderFAQ();
 });
 
+let index = 0;
 function showSlides() {
   index++;
   document.getElementsByClassName(
@@ -13,3 +19,75 @@ function showSlides() {
   }
   setTimeout(showSlides, 4000);
 }
+
+const FAQData = [
+  {
+    title: "Lorem ipsum dolor sit amet",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat.",
+  },
+  {
+    title: "Nunc maximus, magna at ultricies elementum",
+
+    body: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+  },
+  {
+    title: "Curabitur laoreet, mauris vel blandit fringilla",
+
+    body: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncusnunc, ac sagittis leo elit vel lorem.",
+  },
+  {
+    title: "risus turpis vulputate quam, vitae convallis.",
+
+    body: "current version is 1.2.1",
+  },
+];
+
+let visibilities = [];
+
+const initVisibilities = () => {
+  FAQData.forEach(() => {
+    visibilities.push(true);
+  });
+};
+
+const toggleVisibility = (index) => {
+  visibilities[index] = !visibilities[index];
+  renderFAQ();
+};
+
+const renderFAQ = () => {
+  let widgets = [];
+  FAQData.forEach((faq, index) => {
+    const QAComponent = `<div id="component${index}" class="w-full column QA_Content">
+        <div
+          class="QA_Header"
+          onClick={toggleVisibility(${index})}
+        >
+          <div class="row justify-between">
+            <div class="QA_TitleText">
+              ${faq.title}
+            </div>
+            <div
+            class="${visibilities[index] ? "bottomArrowUp" : "bottomArrowDown"}"
+            > <img src="./assets/images/home/bottomArrow.svg" class="bottomArrow"   />
+            </div>
+          </div>
+        </div>
+        <div class= "ans ${
+          visibilities[index] ? "visibleAns" : "inVisibleAns"
+        }">
+          <div
+            class="
+              ${visibilities[index] ? "visibleAnsText" : `invisibleAnsText`}
+            "
+          >
+            ${faq.body}
+          </div>
+        </div>
+      </div>`;
+
+    widgets.push(QAComponent.substring(0, QAComponent.length - 1));
+    const faqs = document.getElementById("faqs");
+    faqs.innerHTML = widgets;
+  });
+};
