6e8279cd89463ff62c1a79eeb131a283595e4eb8
Akalanka Perera <akalankaperera128@gmail.com>
2021-09-09 23:58:45 +0530

    Added image attachment feature to chat

--- modules/chat/userChat/userChat.js
+++ modules/chat/userChat/userChat.js
@@ -44,6 +44,29 @@ const chats = [{
   username: "test username",
   time: Date.now(),
 }];
+
+const encodeImage = (e) => {
+    if (e && e[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = () => {
+          const imgBase64Path = e.target.result;
+          const img = imgBase64Path.split(",")[1];
+          chats.push({
+            message: "base64EncodedImg/imageUrl"+img,
+            userId: "123",
+            username: "test username",
+            time: Date.now(),
+          });
+          renderChatList();
+        };
+      };
+      reader.readAsDataURL(e[0]);
+    }
+  };
+
 const userId="123";
 
 const addToChat = () => {
@@ -66,12 +89,12 @@ const renderChatList = () => {
     const chatTile = `
     <div class="w-full row ${chat.userId == userId ? "justify-end" : "justify-start"}">
     <div class="column justify-end" style="${chat.userId == userId ? "text-align:right; margin-left:25%;" : "text-align:left; margin-right:25%;"}">
-      <div class="w-full row ${chat.userId == userId ?"justify-end":"justify-start"}" style="margin-top:20px;margin-bottom:8px; ${chat.userId == userId ?"margin-right:30px;":"margin-left:30px;"}">
+      <div class="w-full row ${chat.userId == userId ?"justify-end":"justify-start"}" style="margin-top:25px;margin-bottom:8px; ${chat.userId == userId ?"margin-right:30px;":"margin-left:30px;"}">
         ${chat.username}
       </div>
       <div class="w-full row ${chat.userId == userId ?"justify-end":"justify-start"}">
-        <div class="chatTile ${chat.userId == userId ?"sendTile":"receiveTile"}">
-          ${chat.message}
+        <div class="chatTile ${chat.userId == userId ?"sendTile":"receiveTile"}" style="${chat.message.includes('base64EncodedImg/imageUrl')?'background-color:#FFFFFF !important; box-shadow:none !important':''}">
+          ${!chat.message.includes('base64EncodedImg/imageUrl')?chat.message:`<img src="data:image/jpeg;base64,${chat.message.replaceAll('base64EncodedImg/imageUrl','')}" class="chatMessageImage"/>`}
         </div>
       </div>  
       <div class="w-full row ${chat.userId == userId ?"justify-end":"justify-start"}" style="font-size:15px; margin-top:8px; ${chat.userId == userId ?"margin-right:30px;":"margin-left:30px;"}">
@@ -121,6 +144,12 @@ const renderUserChatModal = () => {
               <img
                 src="/Online-Pharmacy-Portal/assets/images/chat/attachment.png"
                 class="attachmentIcon"
+              />
+              <input
+                type="file"
+                id="photo"
+                class="attachmentInput"
+                onChange="encodeImage(this.files)"
               />
             </div>
             <div class="iconCircle row justify-center" onclick="addToChat()">
