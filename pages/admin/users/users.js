56168709326cba1e365dc7e794c1d405892de4ab
Akalanka Perera <akalankaperera128@gmail.com>
2021-09-15 03:44:19 +0530

    User management functionality added

--- pages/admin/users/users.js
+++ pages/admin/users/users.js
@@ -7,13 +7,92 @@ const toggleModal = () => {
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  const formData = {
-    username:e.target.username.value,
-    email:e.target.email.value,
-    userRole:e.target.userRole.value,
-  }
-  console.log(formData)
-  toggleModal();
+  var data = new FormData();
+  data.append("function","addUser");
+  data.append("email", e.target.email.value);
+  data.append("username", e.target.username.value);
+  data.append("password", e.target.password.value);
+  data.append("userRole", e.target.userRole.value);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const result = JSON.parse(this.responseText);
+      if (result.success == true) {
+        toggleModal();
+        let timerInterval
+        Swal.fire({
+          icon: "success",
+          heightAuto: false,
+          background: "#f5fdff",
+          title: `<div style="font-size:23px">${result.message}</div>`,
+          showConfirmButton: false,
+          timer: 1500,
+          willClose: () => {
+            clearInterval(timerInterval)
+          },
+        }).then(() => {
+          window.location.reload();
+        })
+      } else {
+        let errorMessage;
+        if (result.message.includes("Duplicate entry")) {
+          errorMessage = "A user with that email already exists";
+        } else {
+          errorMessage = result.message;
+        }
+
+        Swal.fire({
+          icon: "warning",
+          heightAuto: false,
+          background: "#f5fdff",
+          title: `<div style="font-size:23px">${errorMessage}</div>`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
+  };
+  xmlhttp.open("POST", "users.php", true);
+  xmlhttp.send(data);
+};
+
+const deleteUser = (email) => {
+  var data = new FormData();
+  data.append("function","deleteUser");
+  data.append("email", email);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const result = JSON.parse(this.responseText);
+      if (result.success == true) {
+        let timerInterval
+        Swal.fire({
+          icon: "success",
+          heightAuto: false,
+          background: "#f5fdff",
+          title: `<div style="font-size:23px">${result.message}</div>`,
+          showConfirmButton: false,
+          timer: 1500,
+          willClose: () => {
+            clearInterval(timerInterval)
+          },
+        }).then(() => {
+          window.location.reload();
+        })
+      } else {
+        Swal.fire({
+          icon: "warning",
+          heightAuto: false,
+          background: "#f5fdff",
+          title: `<div style="font-size:23px">${result.message}</div>`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
+  };
+  xmlhttp.open("POST", "users.php", true);
+  xmlhttp.send(data);
 };
 
 const renderModal = () => {
@@ -25,6 +104,8 @@ const renderModal = () => {
       <form
         id="userForm"
         method="post"
+        action="users.php"
+        target="dummyframe"
         class="modalForm column"
       >
         <div class="w-full row justify-end" style="height: 70px">
@@ -48,6 +129,7 @@ const renderModal = () => {
           placeholder="Username"
           type="text"
           id="username"
+          name="username"
           required
         />
         <input
@@ -55,11 +137,21 @@ const renderModal = () => {
           placeholder="Email"
           type="email"
           id="email"
+          name="email"
+          required
+        />
+        <input
+          class="inputField"
+          placeholder="Password"
+          type="password"
+          id="password"
+          name="password"
           required
         />
         <select
           class="inputField"
           id="userRole"
+          name="userRole"
           required
         >
           <option value="" disabled selected>User Role</option>
@@ -90,8 +182,46 @@ const renderModal = () => {
   }
 };
 
+const renderUserList = () =>{
+  var data = new FormData();
+  data.append("function","getUsers");
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const users = JSON.parse(this.responseText);
+      const userElement = document.getElementById("userList");
+      userElement.innerHTML='';
+      users.forEach((user,index) => {
+        const userComponent = `
+            <div class="row userTableRow" data-aos="${index%2==0?"fade-right":"fade-left"}">
+              <div class="tableCell emailCell row justify-start"><img
+              src="/Online-Pharmacy-Portal/assets/images/admin/users/email.svg"
+              class="emailIcon"
+            /><div>${user.email}</div></div>
+              <div class="tableCell usernameCell row justify-start"><img
+              src="/Online-Pharmacy-Portal/assets/images/admin/users/user.png"
+              class="userIcon"
+            /><div>${user.username}</div></div>
+              <div class="tableCell userRoleCell row justify-start"><img
+              src="/Online-Pharmacy-Portal/assets/images/admin/users/userRole.png"
+              class="roleIcon"
+            /><div>${user.userRole}</div></div>
+              <div class="tableCell deleteCell row justify-center"><div onclick="deleteUser('${user.email}')"><img
+              src="/Online-Pharmacy-Portal/assets/images/admin/bin.png"
+              class="binImage"
+            /></div></div>
+            </div>`;
+          userElement.insertAdjacentHTML('beforeend', userComponent);
+      });
+    }
+  };
+  xmlhttp.open("POST", "users.php", true);
+  xmlhttp.send(data);
+}
+
 const initialize = () => {
   AOS.init({ offset: 0, duration: 1000 });
+  renderUserList();
   renderModal();
   document
     .getElementById("productTab")
